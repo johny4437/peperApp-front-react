14d214c156506855bc9105e30f1265ddf1a30806
johny anastacio <johnyanastaciods@gmail.com>
2020-06-27 16:19:17 -0300

    dashboard almost finished

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,52 +1,42 @@
-import React from 'react'
-import './Dashboard.css';
-import FormPrice from '../FormPrice';
-import { useHistory } from "react-router-dom";
-import {singout, isAuthenticated} from  '../../auth/index';
-
+import React from 'react';
+import FormPrice from  '../FormPrice/index'
+import './Dashboard.css'
+import {singout} from  '../../auth/index';
+import {useHistory} from 'react-router-dom'
 
  function Dashboard() {
-    const history = useHistory();
 
-    const headerDash = () =>(
-        <div class="container">
-        <nav>
-            <div class="logo">
-                Admin<span>Panel</span>
-            </div>
-            <ul id="menu">
-                <li><a href=""><i className="fa fa-fw fa-home"></i>Home</a></li>
-                <li><a onclick="changeUrl()"><i class="fa fa-product-hunt"></i>Produtos</a></li>
-                <li><a href=""><i className="fa fa-fw fa-user"></i>Clientes</a></li>
-                <li><a href=""><i className="fa fa-fw fa-wrench"></i>Serviços</a></li>
-                <li><a href=""><i className="fa fa-fw fa-envelope"></i>Contatos</a></li>
-            </ul>
-        </nav>
-        <header>
-            <div id="links">
-                <a href=""><i className="logout"></i>Logout</a>
-            </div>
-        </header>
-
-</div>
-
-
+    const history =  useHistory();
     
-    
-    
-            
-           
-
-           
-        
-    ) 
-
-
-    return (
-        <div className="container">
-            {headerDash()}
-            <FormPrice/>
-        </div>
-    )
+     return(
+         <div>
+             <input type="checkbox" id="menu"/>
+             <nav>
+                 <label >Dashboard</label>
+                 <ul>
+                     <li><a href="#" onClick={()=> singout(()=>{
+                         history.push('/')
+                     })}><i className="fa fa-sign-out"></i>Sair</a></li>
+                 </ul>
+                 <label htmlFor="menu" className="menu-bar">
+                     <i className="fa fa-bars"></i>
+                 </label>
+             </nav>
+             <div className="side-menu">
+                 <a href="#"><i className="fa fa-user"></i><span>Adicionar Preço</span></a>
+                 <a href="#"><i className="fa fa-envelope"></i><span>Últimos preços</span></a>
+                 <a href="#"><i className="fa fa-calendar"></i><span>Preços</span></a>
+                 <a href="#"><i className="fa fa-user"></i><span>Preços</span></a>
+                 <a href="#"><i className="fa fa-user"></i><span>Preços</span></a>
+             
+                 <a href="#" className="logout"><i className="fa fa-sign-out"></i><span>Logout</span></a>
+             
+             </div>
+             <div className="data">
+                 <FormPrice/>
+             </div>
+         </div>
+     )
 }
+
 export default Dashboard;
